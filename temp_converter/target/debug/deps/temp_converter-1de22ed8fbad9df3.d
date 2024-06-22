18b15118e26857415243dd2372a90e3065c2930a
PRiEsTCSC <132480422+PRiEsTCSC@users.noreply.github.com>
2024-06-22 09:48:29 +0100

    Add files via upload

---
+++ temp_converter/target/debug/deps/temp_converter-1de22ed8fbad9df3.d
@@ -0,0 +1,5 @@
+/home/the_priest/Desktop/Programs/RUST/temp_converter/target/debug/deps/libtemp_converter-1de22ed8fbad9df3.rmeta: src/main.rs
+
+/home/the_priest/Desktop/Programs/RUST/temp_converter/target/debug/deps/temp_converter-1de22ed8fbad9df3.d: src/main.rs
+
+src/main.rs:
---
+++ temp_converter/target/debug/deps/temp_converter-97e9a16b0026bfa9.d
@@ -0,0 +1,5 @@
+/home/the_priest/Desktop/Programs/RUST/temp_converter/target/debug/deps/libtemp_converter-97e9a16b0026bfa9.rmeta: src/main.rs
+
+/home/the_priest/Desktop/Programs/RUST/temp_converter/target/debug/deps/temp_converter-97e9a16b0026bfa9.d: src/main.rs
+
+src/main.rs:
