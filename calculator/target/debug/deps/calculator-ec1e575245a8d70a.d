43cab46472af49e64dae182796481697b6922bdb
PRiEsTCSC <132480422+PRiEsTCSC@users.noreply.github.com>
2024-06-22 09:47:21 +0100

    Add files via upload

---
+++ calculator/target/debug/deps/calculator-ec1e575245a8d70a.d
@@ -0,0 +1,5 @@
+/home/the_priest/Desktop/Programs/RUST/calculator/target/debug/deps/libcalculator-ec1e575245a8d70a.rmeta: src/main.rs
+
+/home/the_priest/Desktop/Programs/RUST/calculator/target/debug/deps/calculator-ec1e575245a8d70a.d: src/main.rs
+
+src/main.rs:
